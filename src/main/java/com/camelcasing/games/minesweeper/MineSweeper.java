83bfd83d5cc72aebeae588537d0662a51d63fd76
Philip Teclaff <philipteclaff@gmail.com>
2014-08-11 09:00:15 +0100

    inital commit

---
+++ src/main/java/com/camelcasing/games/minesweeper/MineSweeper.java
@@ -0,0 +1,16 @@
+package com.camelcasing.games.minesweeper;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MineSweeper extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception{
+		
+	}
+	
+	public static void main(String[] args){
+		Application.launch(args);
+	}
+}
