36265059d394bce8ff62feb604b9bf4e9b181468
Philip Teclaff <philipteclaff@gmail.com>
2014-08-12 14:05:34 +0100

    working game!

--- src/main/java/com/camelcasing/games/minesweeper/GridSquare.java
+++ src/main/java/com/camelcasing/games/minesweeper/GridSquare.java
@@ -1,6 +1,7 @@
 package com.camelcasing.games.minesweeper;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.*;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -10,14 +11,20 @@ import javafx.scene.paint.Color;
 public class GridSquare{
 
 		private Button gridSquare;
-		private int row;
-		private int column;
+		private int row = -1;
+		private int column = -1;
 		private boolean isMine = false;
-		private int nextToCount = -1;
+		private boolean markedAsBomb = false;
+		private int nextToCount = 0;
+		private int size = 30;
+		private ImageView redFlag;
+		private boolean revealed = false;
 		
-	public GridSquare(int row, int column){
+	public GridSquare(int row, int column, ImageView redFlag){
 		this.row = row;
 		this.column = column;
+		this.redFlag = redFlag;
+		//gridSquare.setGraphic(redFlag);
 		gridSquare = new Button();
 		gridSquare.setBorder(new Border(new BorderStroke(
 				Color.BLACK, BorderStrokeStyle.DOTTED, new CornerRadii(5), BorderStroke.THIN)));
@@ -26,8 +33,8 @@ public class GridSquare{
 				Color.BLACK, BorderStrokeStyle.DOTTED, new CornerRadii(5), BorderStroke.THIN))));
 		gridSquare.setOnMouseEntered(me -> gridSquare.setBorder(new Border(new BorderStroke(
 				Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5), BorderStroke.MEDIUM))));
-		gridSquare.setMinSize(30, 30);
-		gridSquare.setMaxSize(30, 30);
+		gridSquare.setMinSize(size, size);
+		gridSquare.setMaxSize(size, size);
 	}
 	
 	public boolean isMine(){
@@ -38,8 +45,8 @@ public class GridSquare{
 		this.isMine = isMine;
 	}
 
-	public int getNextToCount(){
-		return nextToCount;
+	public String getNextToCount(){
+		return Integer.toString(nextToCount);
 	}
 
 	public void setNextToCount(int nextToCount){
@@ -50,11 +57,36 @@ public class GridSquare{
 		return gridSquare;
 	}
 	
+	public void setGridSquare(Button b){
+		this.gridSquare = b;
+	}
+	
+	public void markAsBomb(){
+		markedAsBomb = true;
+	}
+	
+	public void unmarkAsBomb(){
+		markedAsBomb = false;
+	}
+	
 	public int getRow(){
 		return row;
 	}
 	
+	public void reveal(){
+		revealed = true;
+	}
+	
+	public boolean isRevealed(){
+		return revealed;
+	}
+	
 	public int getColumn(){
 		return column;
+	}
+	
+	@Override
+	public String toString(){
+		return "index " + row + " X " + column + (isMine ? " is a mine" : " not a mine and next to " + nextToCount + " mines");
 	}
 }
--- src/main/java/com/camelcasing/games/minesweeper/MineSweeper.java
+++ src/main/java/com/camelcasing/games/minesweeper/MineSweeper.java
@@ -1,19 +1,23 @@
 package com.camelcasing.games.minesweeper;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.apache.log4j.Logger;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.geometry.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 public class MineSweeper extends Application{
 
-		private final static Logger logger = Logger.getLogger(MineSweeper.class.getName());
+		public final static Logger logger = Logger.getLogger(MineSweeper.class.getName());
 	
 		private int boardWidth = 16;
 		private int boardHeight = 16;
@@ -21,17 +25,25 @@ public class MineSweeper extends Application{
 		private int numberOfMines;
 		private GridSquare[][] buttons;
 		private GridPane backPanel;
-		
+		private BorderPane back;
+		private Random rnd = new Random();
+		private ImageView redFlag;
+		private Stage stage;
+		private int remainingBombs; 
+		private Label label;
 		
 	@Override
 	public void start(Stage stage) throws Exception{
+		
+		this.stage = stage;
+		//redFlag = new ImageView(new Image("file:/src/main/resources/red_flag_30x30.png"));
 		
 		createGUI();
 		createGrid();
 		assignBombs();
 		assignNumbers();
 		
-		Scene root = new Scene(backPanel);
+		Scene root = new Scene(back);
 		root.getStylesheets().add("stylesheet.css");
 		stage.setScene(root);
 		stage.setTitle("Mine Sweeper");
@@ -39,9 +51,17 @@ public class MineSweeper extends Application{
 	}
 	
 	private void createGUI(){
+		back = new BorderPane();
 		backPanel = new GridPane();
 		backPanel.setAlignment(Pos.CENTER);
 		backPanel.setPadding(new Insets(30));
+		label = new Label();
+		label.setPadding(new Insets(10, 0, 0, 0));
+		HBox hbox = new HBox();
+		hbox.setAlignment(Pos.CENTER);
+		hbox.getChildren().add(label);
+		back.setCenter(backPanel);
+		back.setTop(hbox);
 	}
 	
 	private void createGrid(){
@@ -56,7 +76,23 @@ public class MineSweeper extends Application{
 						b.setBorder(new Border(new BorderStroke(
 								Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(5), BorderStroke.THIN)));
 					});
+
 					checkSquare(gs);
+				});
+				gs.getGridSquare().setOnMouseClicked(me -> {
+					if(me.getButton() == javafx.scene.input.MouseButton.SECONDARY){
+						Button b = (Button) me.getSource();
+						if(b.getText().equals("")){
+							b.setStyle("-fx-text-fill: #ff0000");
+							b.setText("F");
+							remainingBombs--;
+							label.setText("Bombs remaining = " + remainingBombs);
+						}else if(b.getText().equals("F")){
+							b.setText("");
+							remainingBombs++;
+							label.setText("Bombs remaining = " + remainingBombs);
+						}
+					}
 				});
 				backPanel.add(buttons[i][j].getGridSquare(), i, j);
 			}
@@ -64,30 +100,88 @@ public class MineSweeper extends Application{
 	}
 	
 	private GridSquare createGridSquare(int row, int column){
-		GridSquare gridSquare = new GridSquare(row, column);
+		GridSquare gridSquare = new GridSquare(row, column, redFlag);
 
 		return gridSquare;
 	}
 	
 	private void checkSquare(GridSquare gridSquare){
+		if(gridSquare.getGridSquare().getText().equals("F")) return;
 		if(gridSquare.isMine()) gameOver();
-		
+		String s = gridSquare.getNextToCount();
+		gridSquare.getGridSquare().setStyle("-fx-background-color: #ffffff");
+		gridSquare.getGridSquare().setText(s);
 	}
 	
 	private void gameOver(){
 		logger.debug("GameOver");
+		JOptionPane.showMessageDialog(null, "GAMEOVER");
+		reset();
 	}
 	
 	private void assignBombs(){
+		logger.debug("bombAssignment started");
 		totalSquares = boardWidth * boardHeight;
-		numberOfMines = totalSquares / 5;
+		numberOfMines = totalSquares / 6;
+		remainingBombs = numberOfMines;
+		label.setText("Bombs remaining = " + remainingBombs);
+		int w = -1;
+		int h = -1;
 			if(logger.isDebugEnabled()){
 				logger.debug("created " + boardWidth + " X " + boardHeight + " board with " + numberOfMines + " mines");
 			}
+		for(int i = 0; i < numberOfMines; i++){
+				do{
+					w = rnd.nextInt(boardWidth);
+					h = rnd.nextInt(boardHeight);
+				}while(buttons[w][h].isMine());
+			buttons[w][h].setMine(true);
+		}
+		logger.debug("bombAssignment completed");
 	}
 	
 	private void assignNumbers(){
+		logger.debug("numberAssignment started");
+		for(int i = 0; i < buttons.length; i++){
+			assign:
+			for(int j = 0; j < buttons[i].length; j++){
+					if(buttons[i][j].isMine()){
+						buttons[i][j].setNextToCount(-1);
+						continue assign;
+					}
+					int count = 0;
+						GridSquare gs;
+						if((gs = getFromIndex(i-1, j)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i-1, j+1)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i, j+1)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i+1, j+1)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i+1, j)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i+1, j-1)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i, j-1)) != null && gs.isMine()) count++;
+						if((gs = getFromIndex(i-1, j-1)) != null && gs.isMine()) count++;
+					buttons[i][j].setNextToCount(count);
+			}
+		}
+		logger.debug("numberAssignment completed");
+	}
+	
+	public GridSquare getFromIndex(int r, int c){
+		try{
+			return buttons[r][c];
+		}catch(ArrayIndexOutOfBoundsException e){
+			return null;
+		}
+	}
+	
+	public void reset(){
+		createGUI();
+		createGrid();
+		assignBombs();
+		assignNumbers();
 		
+		Scene root = new Scene(back);
+		root.getStylesheets().add("stylesheet.css");
+		stage.setScene(root);
 	}
 	
 	public static void main(String[] args){
