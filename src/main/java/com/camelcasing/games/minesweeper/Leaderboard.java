3fa5f88ff0346d7ee9cf0ab013794889c6b12617
Philip Teclaff <philipteclaff@gmail.com>
2014-09-26 03:47:19 +0100

    updated leaderboard to use javafx and with proper formatting

--- src/main/java/com/camelcasing/games/minesweeper/Leaderboard.java
+++ src/main/java/com/camelcasing/games/minesweeper/Leaderboard.java
@@ -2,7 +2,12 @@ package com.camelcasing.games.minesweeper;
 
 import java.util.prefs.Preferences;
 
-import javax.swing.JOptionPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class Leaderboard{
 
@@ -22,16 +27,37 @@ public class Leaderboard{
 	}
 
 	public void displayLeaderboard(){
-		MineSweeper.logger.debug("\nLargeBest = " + largeBest + "\n" + 
-				"MediumBest = " + mediumBest + "\n" +
-				"SmallBest = " + smallBest);
-		JOptionPane.showMessageDialog(null, "LargeBest = " + largeBest + "\n" + 
-				"MediumBest = " + mediumBest + "\n" +
-				"SmallBest = " + smallBest);
+		MineSweeper.logger.debug("\nLargeBest = " + convertSeconds(largeBest) + "\n" + 
+				"MediumBest = " + convertSeconds(mediumBest) + "\n" +
+				"SmallBest = " + convertSeconds(smallBest));
+		
+		Text text = new Text("Large Board \t " + convertSeconds(largeBest) + "\n\n" + 
+				"Medium Board \t " + convertSeconds(mediumBest) + "\n\n" +
+				"Small Board \t " + convertSeconds(smallBest));
+		text.setFont(new Font("Ubuntu", 24));
+		
+		HBox root = new HBox();
+		root.setAlignment(Pos.CENTER);
+		HBox.setMargin(text, new Insets(50, 50, 50, 50));
+		
+		root.getChildren().add(text);
+		
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.show();
 	}
 
 	public int getLargeBest() {
 		return largeBest;
+	}
+	
+	public String convertSeconds(int seconds){
+		if(seconds == 1000000000) return "Not Set";
+		int hours = seconds / 3600;
+		seconds = seconds - hours * 3600;
+		int minutes = seconds / 60;
+		seconds = seconds - minutes * 60;
+		return String.format("%02d:%02d:%02d", hours, minutes, seconds);
 	}
 
 	public boolean setLargeBest(int largeBest){
